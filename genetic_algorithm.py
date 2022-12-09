3442a0f57a64f97ba5c0a43feaed1a5edcd7b494
Kemal Demirel <kmldemirel99@gmail.com>
2022-12-09 15:07:54 +0300

    Update on genetic algorithm

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,47 +1,82 @@
 
 
-from random import randint
-import numpy as np
-import matplotlib.pyplot as plt
 
-class GeneticAlgorithm:
-    
-    
-    def __init__(self):
-        self.n = 10                             #
-        self.a = 1023                           #
-        self.c = 0                              # 
-        self.l = 10                             # Length of the binary string
-        self.yz = 91                            # The last two digits of your studentID
-        self.t = 1000                           # Number of iterations
-        self.p_cross = (0.50 + self.yz /100)    # The probability of crossover
-        self.p_mut = (0.003 + self.yz/10000)    # The probability of mutation
-        
-    def recombination(self):
-        pass
-    
-    def crossover(self):
-        pass
-    
-    def mutation(self):
-        pass
-    
-    def election(self):
-        # first random selection
-        #selection_ix = randint(len(pop))
-        #for ix in randint(0, len(pop), k-1):
-        #    # check if better (e.g. perform a tournament)
-        #    if scores[ix] < scores[selection_ix]:
-        #        selection_ix = ix
-        #return pop[selection_ix]
-        election_no = randint(1023)
-        
-    
-    def runGeneticAlgorithm(self):
-        for i in range(self.t):
-            self.recombination()
-            self.crossover()
-            self.mutation()
-            self.election()
+from numpy.random import randint
+from numpy.random import rand
 
-GeneticAlgorithm()
+
+def selection(pop, scores, k=3):
+    selection_ix = randint(len(pop))
+    for ix in randint(0, len(pop), k-1):
+        if scores[ix] < scores[selection_ix]:
+            selection_ix = ix   
+    return pop[selection_ix]
+
+def crossover(p1, p2, r_cross):
+    c1, c2 = p1.copy(), p2.copy()
+    if rand() < r_cross:
+        pt = randint(1, len(p1)-2)
+        c1 = p1[:pt] + p2[pt:]
+        c2 = p2[:pt] + p1[pt:]
+    return [c1, c2]
+
+def mutation(bitstring, r_mut):
+    for i in range(len(bitstring)):
+        if rand() < r_mut:
+            bitstring[i] = 1 - bitstring[i]
+            
+def objective(x):
+    	return x[0]**2.0 + x[1]**2.0
+
+def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut):
+	pop = [randint(0, 2, n_bits).tolist() for _ in range(n_pop)]
+	best, best_eval = 0, objective(pop[0])
+	for gen in range(n_iter):
+		scores = [objective(c) for c in pop]
+		for i in range(n_pop):
+			if scores[i] < best_eval:
+				best, best_eval = pop[i], scores[i]
+				print(">%d, new best f(%s) = %.3f" % (gen,  pop[i], scores[i]))
+		selected = [selection(pop, scores) for _ in range(n_pop)]
+		children = list()
+		for i in range(0, n_pop, 2):
+			p1, p2 = selected[i], selected[i+1]
+			for c in crossover(p1, p2, r_cross):
+				mutation(c, r_mut)
+				children.append(c)
+		pop = children
+	return [best, best_eval]
+
+def onemax(x):
+    return -sum(x)
+
+
+n = 10                             # Number of chromosomes
+a = 1023                           #
+c = 0                              # 
+l = 10                             # Length of the binary string
+yz = 91                            # The last two digits of your studentID
+t = 1000                           # Number of iterations
+p_cross = (0.50 + yz /100)    # The probability of crossover
+p_mut = (0.003 + yz/10000)    # The probability of mutation
+
+
+
+
+# define the total iterations
+#n_iter = 100
+# bits
+#n_bits = 20
+# define the population size
+#n_pop = 100
+# crossover rate
+#r_cross = 0.9
+# mutation rate
+#r_mut = 1.0 / float(n_bits)
+
+
+best, score = genetic_algorithm(onemax, l, t, n, p_cross, p_mut)
+print('Done!')
+print('f(%s) = %f' % (best, score))
+
+
